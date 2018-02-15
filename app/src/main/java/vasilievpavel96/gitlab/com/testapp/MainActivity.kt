ffcfcfba1d16d5372b7cfdbb86f2493904e93781
Pavel Vasiliev <Vasilievpavel96@gmail.com>
2018-02-15 22:52:08 +0300

    init

---
+++ app/src/main/java/vasilievpavel96/gitlab/com/testapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package vasilievpavel96.gitlab.com.testapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
